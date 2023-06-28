dd4ea984bee5ded96bd1fd75ab96af622a3dabc9
Brian Duarte <braianmucioduarte@gmail.com>
2023-06-28 11:49:42 -0300

    Upgrade in js and style

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function reset () {
     for (let i in square) {
         square[i] = '';
     }
-
+    clearBoardStyle();
     renderSquare();
     renderInfo();
     updateScore();
@@ -81,13 +81,20 @@ function renderSquare() {
     for(let i in square) {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
+        item.classList.remove('x', 'o');
+
+        if(square[i] === 'x') {
+            item.classList.add('x');
+        } else if (square[i] === 'o') {
+            item.classList.add('o');
+        }
     }
 
     checkGame();
 }
 
 function renderInfo() {
-    document.querySelector('.vez').innerHTML = player;
+    //document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
 }
 
@@ -104,10 +111,12 @@ function checkGame() {
         warning = 'O "x" foi o vencedor!';
         score.x++; // Incrementa a pontuação do jogador 'x'
         playing = false;
+        applyWinnerStyle('x');
     } else if (checkWinnerFor('o')) {
         warning = 'O "o" foi o vencedor!';
         score.o++; // Incrementa a pontuação do jogador 'o'
         playing = false;
+        applyWinnerStyle('o');
     } else if (isFull()) {
         warning = 'Vocês empataram!';
         playing = false;
@@ -140,6 +149,45 @@ function checkWinnerFor(player) {
 
     return false;
 }
+
+function applyWinnerStyle(player) {
+    let pos = [
+      // Sequências de vitória na horizontal
+      ['a1', 'a2', 'a3'],
+      ['b1', 'b2', 'b3'],
+      ['c1', 'c2', 'c3'],
+      // Sequências de vitória na vertical
+      ['a1', 'b1', 'c1'],
+      ['a2', 'b2', 'c2'],
+      ['a3', 'b3', 'c3'],
+      // Sequências de vitória nas diagonais
+      ['a1', 'b2', 'c3'],
+      ['a3', 'b2', 'c1'],
+    ];
+  
+    for (let w in pos) {
+      let pArray = pos[w];
+      let hasWon = pArray.every((option) => square[option] === player);
+      if (hasWon) {
+        pArray.forEach((option) => {
+          let item = document.querySelector(`div[data-item=${option}]`);
+          item.classList.add('winner'); // Adiciona a classe 'victory' às células da sequência de vitória
+        });
+        break;
+      }
+    }
+}
+  
+// retirando o plano de fundo do vencedor
+function clearBoardStyle() {
+    const cells = document.querySelectorAll('.item');
+    cells.forEach((cell) => {
+      cell.classList.remove('winner');
+    });
+  }
+  
+  
+  
 
 function isFull() {
     for( let i in square) {
